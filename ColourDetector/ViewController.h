cdab84151504137560c7cd15805bc62044141f3f
Chris Greening <chris@cmgresearch.com>
2011-10-06 11:37:17 +0100

    Initial Commit

---
+++ ColourDetector/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ColourDetector
+//
+//  Created by Chris Greening on 06/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ColourDetector/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ColourDetector
+//
+//  Created by Chris Greening on 06/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
